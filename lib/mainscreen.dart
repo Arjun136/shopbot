0dbe9f81a5925b06d90f0c72d430dff3a6cbf78d
Arjun136 <98836118+Arjun136@users.noreply.github.com>
2022-02-07 22:09:27 +0400

    Update mainscreen.dart

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,40 +1,53 @@
 import 'package:flutter/material.dart';
 import 'secondscreen.dart';
+import 'thirdscreen.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       appBar: AppBar(
         leading: IconButton(icon: Icon(Icons.menu), onPressed: () {
           //
         }),
         title: Text("Shopbot"),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search), onPressed: () {
+          IconButton(icon: Icon(Icons.menu), onPressed: () {
             //
           }),
         ],
       ),
+
       body: Center(
         child: ElevatedButton(
-          child: Text("Chat Bot",
-          style: TextStyle(
-              color: Colors.white,
-
-
-          ),
+          child: Text("Chatbot",
+            style: TextStyle(
+              color: Colors.black,
+            ),
           ),
           onPressed: () {
             Navigator.push(context,
-            MaterialPageRoute(builder: (context) => SecondScreen()),
-
+              MaterialPageRoute(builder: (context) => SecondScreen()),
 
             );
           },
         ),
+
       ),
+    floatingActionButton: FloatingActionButton(
+    onPressed: () {
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => ThirdScreen()),
+      );
+      },
+    backgroundColor: Colors.teal,
+    )
+
     );
   }
 }
+
+
+
 
 
